26db0291bab3fb6a82a05e849bc3bea14c549641
Nahin-CDR <nahin.cdr@gmail.com>
2025-10-16 15:35:34 +0600

    msg changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-      <h1>Hello World from Next.js!</h1>
+      <h1>Hello World from Auto Deploy</h1>
     </div>
   );
 }
