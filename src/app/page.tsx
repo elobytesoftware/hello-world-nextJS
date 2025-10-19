4b41b8e9c33a3426540f12e63eb70b1ae0d25ee4
Nahin-CDR <nahin.cdr@gmail.com>
2025-10-19 12:59:45 +0600

    name 7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <div>
       <h1>Hello World from Auto Deploy</h1>
-      <h1>Deployed by -- Nahin</h1>
+      <h1>Deployed by -- 0 Nahin</h1>
     </div>
   );
 }
