fbedf20bbddc6de0212ac9388720ba7621de76cd
Nahin-CDR <nahin.cdr@gmail.com>
2025-10-21 12:35:50 +0600

    code optimized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-      <h1>Hello World from Auto Deploy</h1>
+      <h1>Hello World</h1>
       <h1>Deployed by - Nahin </h1>
     </div>
   );
