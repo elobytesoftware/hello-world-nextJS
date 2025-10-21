6f2652e1bea058bc5c884e638f063151ecf4d903
Nahin-CDR <nahin.cdr@gmail.com>
2025-10-21 12:48:50 +0600

    text changed in UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-      <h1>Hello World</h1>
+      <h1>Hello World : V1</h1>
       <h1>Deployed by - Nahin </h1>
     </div>
   );
