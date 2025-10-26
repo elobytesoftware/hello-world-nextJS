758b66ddec536079db0c51fc966a3f84cf51686d
Nahin-CDR <nahin.cdr@gmail.com>
2025-10-26 20:20:49 +0600

    v6 added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-      <h1>Hello World : V5 </h1>
+      <h1>Hello World : V6 </h1>
       <h1>CI -CD demo - 2</h1>
       <h1>Deployed by - Nahin </h1>
       <h2>Date : 26-10-2025</h2>
